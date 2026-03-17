39614af6387be72cee3de6d368acdcc31af4eff4
Ines Rivero Garcia <inesriverog@gmail.com>
2026-03-17 10:58:36 +0100

    Added script for mapping QC with RSeQC

--- code/01_preprocessing/01.5_Preprocessing_RSeQC_MappingQC.sh
+++ code/01_preprocessing/01.5_Preprocessing_RSeQC_MappingQC.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
 # Date of creation: 2023/09/22.
-# Date of last modification: 2023/09/22.
-
-## default variables
-while getopts i:o:p:r: flag
-do
-    case "${flag}" in 
-        p) projectdir=${OPTARG};; # NEEDED: /data_lab_MT/Ines/REANIMA_SlamSeq
-        r) refbed=${OPTARG};; # NEEDED: Reference bed file (/data_lab_MT/Ines/REANIMA_SLAMseq_Experiments/Mmusculus_GRCm39_Ensembl106_GenomeFiles/Mus_musculus.GRCm39.106.gtf_galaxy.bed)
-    esac
-done
+# Date of last modification: 2026/03/17.
 
 ## creating paths
-projectdir="/data_lab_MT/Ines/REANIMA_SlamSeq"
-datadir="$projectdir/Aligned.Reads"
-outdir="$datadir/RSeQC"
+projectdir="/Volumes/INTENSO/PhD/cardio_slamseq"
+datadir="$projectdir/data/Aligned.Reads"
+outdir="$projectdir/results/01_preprocessing/RSeQC"
+refanno="$projectdir/reference/GRAND-SLAM/Mus_musculus.GRCm39.106.gtf"
+genepred="$projectdir/reference/RSeQC/Mus_musculus.GRCm39.106.genePred"
+refbed="$projectdir/reference/RSeQC/Mus_musculus.GRCm39.106.bed12"
+refbedsorted="$projectdir/reference/RSeQC/Mus_musculus.GRCm39.106.sorted.bed12"
 
 echo ">>> Running 01.5_Preprocessing_RSeQC_MappingQC.sh script" || exit 100 &&
-echo "source ~/miniconda3.cluster.source && conda activate RSeQC &&" || exit 100 && 
-source ~/miniconda3.cluster.source && conda activate RSeQC || exit 100 &&
 
 echo "Program versions" || exit 100 &&
 bam_stat.py --version || exit 100 &&
@@ -27,7 +20,6 @@ mismatch_profile.py --version || exit 100 &&
 read_distribution.py --version || exit 100 &&
 read_duplication.py --version || exit 100 &&
 read_GC.py --version || exit 100 &&
-tin.py --version || exit 100 &&
 
 date +"%F %X" || exit 100 &&
 
@@ -44,6 +36,13 @@ echo "#######################################################################" |
 echo "" || exit 100 &&
 echo "" || exit 100 &&
 
+echo "Preparing annotation bed" || exit 100 &&
+echo "gtfToGenePred $refanno $genepred" || exit 100 &&
+gtfToGenePred "$refanno" "$genepred" || exit 100 &&
+echo "genePredToBed $genepred $refbed" || exit 100 &&
+genePredToBed "$genepred" "$refbed"  || exit 100 &&
+echo "sort -k1,1 -k2,2n $refbed > $refbedsorted" || exit 100 &&
+sort -k1,1 -k2,2n "$refbed" > "$refbedsorted"
 
 for source_file in "$datadir"/*Aligned.sortedByCoord.out.bam; do
     echo "$source_file"
@@ -53,12 +52,12 @@ for source_file in "$datadir"/*Aligned.sortedByCoord.out.bam; do
         echo ">>> Running QC for Sample $inputfile"
         echo "bam_stat.py -i $datadir/$inputfile -q 30 > '$outdir/$outputfile._RSeQC_BamStats.txt'"
         bam_stat.py -i $datadir/$inputfile -q 30 > "$outdir/$outputfile._RSeQC_BamStats.txt"
-        echo "geneBody_coverage.py -r $refbed -i $datadir/$inputfile  -o $outdir/$outputfile"
-        geneBody_coverage.py -r $refbed -i $datadir/$inputfile  -o "$outdir/$outputfile"
-        echo "mismatch_profile.py -l 75 -i $datadir/$inputfile -o $outdir/$outputfile -n 50000000 -q 30"
-        mismatch_profile.py -l 75 -i $datadir/$inputfile -o "$outdir/$outputfile" -n 50000000 -q 30
-        echo "read_distribution.py -i $datadir/$inputfile -r $refbed > '$outdir/$outputfile.RSeQC_ReadDistribution.txt'"
-        read_distribution.py -i $datadir/$inputfile -r $refbed > "$outdir/$outputfile.RSeQC_ReadDistribution.txt"
+        echo "geneBody_coverage.py -r $refbedsorted -i $datadir/$inputfile  -o $outdir/$outputfile"
+        geneBody_coverage.py -r $refbedsorted -i $datadir/$inputfile  -o "$outdir/$outputfile"
+        echo "mismatch_profile.py -l 100 -i $datadir/$inputfile -o $outdir/$outputfile -n 50000000 -q 30"
+        mismatch_profile.py -l 100 -i $datadir/$inputfile -o "$outdir/$outputfile" -n 50000000 -q 30
+        echo "read_distribution.py -i $datadir/$inputfile -r $refbedsorted > '$outdir/$outputfile.RSeQC_ReadDistribution.txt'"
+        read_distribution.py -i $datadir/$inputfile -r $refbedsorted > "$outdir/$outputfile.RSeQC_ReadDistribution.txt"
         echo "read_duplication.py -i $datadir/$inputfile -o $outdir/$outputfile -u 500 -q 30"
         read_duplication.py -i $datadir/$inputfile -o "$outdir/$outputfile" -u 500 -q 30
         echo "read_GC.py -i $datadir/$inputfile -o $outdir/$outputfile -q 30"
