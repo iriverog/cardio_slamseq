2a5c3a51cc8762203130c288782e2a9d678693f1
Ines Rivero Garcia <inesriverog@gmail.com>
2026-03-17 10:49:23 +0100

    Set final paths

--- code/01_preprocessing/01.1_Preprocessing_TrimAdapters_Cutadapt.sh
+++ code/01_preprocessing/01.1_Preprocessing_TrimAdapters_Cutadapt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Date of creation: 2023/02/15.
-# Date of last modification: 2023/09/19.
+# Date of last modification: 2026/03/10.
 
 echo ">>> Running 01.1_Preprocessing_TrimAdapters_Cutadapt.sh script" || exit 100 &&
 date +"%F %X" || exit 100 && 
 
 
-source="/data_lab_MT/Ines/REANIMA_SlamSeq/Raw.Reads"
-destination="/data_lab_MT/Ines/REANIMA_SlamSeq/Processed.Reads"
+source="/Volumes/INTENSO/PhD/cardio_slamseq/data/Raw.Reads"
+destination="/Volumes/INTENSO/PhD/cardio_slamseq/data/Processed.Reads"
 
 for source_file in "$source"/*.gz; do
     if [ -f "$source_file" ]; then
@@ -23,10 +23,6 @@ done
 echo ">>> QC after trimming adapters" || exit 100 &&
 echo "fastqc $destination/*" || exit 100 &&
 fastqc "$destination"/* || exit 100 &&
-
-echo ">>> Preparing MultiQC report" || exit 100 &&
-echo "multiqc $destination" || exit 100 &&
-multiqc "$destination" || exit 100 &&
 
 echo "DONE" || exit 100 &&
 date +"%F %X"
--- code/01_preprocessing/01.4_Preprocessing_Samtools_IndexBam.sh
+++ code/01_preprocessing/01.4_Preprocessing_Samtools_IndexBam.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 # Date of creation: 2023/09/22.
-# Date of last modification: 2023/10/11.
+# Date of last modification: 2026/03/13.
 
-echo ">>> Running 01.3_Preprocessing_Samtools_IndexBam.sh script" || exit 100 &&
-~/samtools-1.15/bin/samtools --version|| exit 100 &&
+echo ">>> Running 01.4_Preprocessing_Samtools_IndexBam.sh script" || exit 100 &&
+
+#echo "conda activate slamseq" || exit 100 &&
+#conda init || exit 100 &&
+#conda activate slamseq || exit 100 &&
+
+samtools --version || exit 100 &&
+
 date +"%F %X" || exit 100 && 
 
-datadir="/data_lab_MT/Ines/REANIMA_SlamSeq/Aligned.Reads"
+datadir="/Volumes/INTENSO/PhD/cardio_slamseq/data/Aligned.Reads"
 
 for source_file in "$datadir"/*Aligned.sortedByCoord.out.bam; do
-    echo "~/samtools-1.15/bin/samtools index $source_file"
-    ~/samtools-1.15/bin/samtools index $source_file
+    echo "samtools index $source_file"
+    samtools index $source_file
     date +"%F %X"
 done
+
+echo "DONE!"
